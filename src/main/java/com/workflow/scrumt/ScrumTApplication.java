d902a6287f0c95d783beaf682a9b6a0353da1c20
Mikael Bernardes <mikaelbernardes2022@gmail.com>
2024-12-27 21:37:12 -0300

    first commit

---
+++ src/main/java/com/workflow/scrumt/ScrumTApplication.java
@@ -0,0 +1,13 @@
+package com.workflow.scrumt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrumTApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workflow/scrumt/ScrumTApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workflow.scrumt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrumTApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
