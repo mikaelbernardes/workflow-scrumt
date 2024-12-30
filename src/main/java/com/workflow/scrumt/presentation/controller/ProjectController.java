c145ec40379c76300cd56bdf034c693f469ef0b7
Mikael Bernardes <mikaelbernardes2022@gmail.com>
2024-12-30 15:09:15 -0300

    feat: add project controller

--- src/main/java/com/workflow/scrumt/presentation/controller/ProjectController.java
+++ src/main/java/com/workflow/scrumt/presentation/controller/ProjectController.java
@@ -1,15 +1,17 @@
 package com.workflow.scrumt.presentation.controller;
 
 import com.workflow.scrumt.application.dto.ProjectDTO;
+import com.workflow.scrumt.application.dto.UserDTO;
 import com.workflow.scrumt.application.service.ProjectService;
 import com.workflow.scrumt.domain.entity.Project;
+import com.workflow.scrumt.domain.entity.User;
 import com.workflow.scrumt.presentation.response.ProjectResponse;
+import com.workflow.scrumt.presentation.response.UserResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/project")
@@ -30,4 +32,39 @@ public class ProjectController {
         return ResponseEntity.ok(response);
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<ProjectResponse> updateProject(
+            @PathVariable Long id,
+            @RequestBody ProjectDTO projectDTO
+    ){
+        Project updateProject = projectService.updateProject(id, projectDTO);
+        ProjectResponse response = new ProjectResponse(
+                updateProject.getId(),
+                updateProject.getName(),
+                updateProject.getDescription(),
+                updateProject.getCreatedAt()
+        );
+        return ResponseEntity.ok(response);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<ProjectResponse> patchProject(
+            @PathVariable Long id,
+            @RequestBody Map<String, Object> updates
+    ) {
+        Project patchedUser = projectService.patchProject(id, updates);
+        ProjectResponse response = new ProjectResponse(
+                patchedUser.getId(),
+                patchedUser.getName(),
+                patchedUser.getDescription(),
+                patchedUser.getCreatedAt()
+        );
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProject(@PathVariable Long id) {
+        projectService.deleteProject(id);
+        return ResponseEntity.noContent().build();
+    }
 }
