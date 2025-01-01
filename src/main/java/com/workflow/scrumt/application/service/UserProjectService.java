0f88c2cfbd178e78f73b6cef3a1ed4c0bd9f85f3
Mikael Bernardes <mikaelbernardes2022@gmail.com>
2025-01-01 12:22:59 -0300

    feat: add validation for user role

--- src/main/java/com/workflow/scrumt/application/service/UserProjectService.java
+++ src/main/java/com/workflow/scrumt/application/service/UserProjectService.java
@@ -4,9 +4,13 @@ import com.workflow.scrumt.application.dto.UserProjectDTO;
 import com.workflow.scrumt.application.mapper.UserProjectMapper;
 import com.workflow.scrumt.application.validation.UserProjectValidation;
 import com.workflow.scrumt.domain.entity.UserProject;
+import com.workflow.scrumt.domain.enums.UserRole;
+import com.workflow.scrumt.domain.exceptions.CustomException;
+import com.workflow.scrumt.domain.exceptions.ExceptionLevel;
 import com.workflow.scrumt.domain.repository.UserProjectRepository;
 import com.workflow.scrumt.domain.useCase.userProject.PatchUserProjectRoleUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -23,7 +27,18 @@ public class UserProjectService implements PatchUserProjectRoleUseCase {
     public UserProject patchUserProjectRole(Long id, UserProjectDTO userProjectDTO) {
         userProjectValidation.validatePatchRole(id, userProjectDTO);
         UserProject existingUserProject = userProjectRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("UserProject not found"));
+                .orElseThrow(() -> new CustomException("UserProject not found", ExceptionLevel.ERROR, HttpStatus.NOT_FOUND));
+
+        if (existingUserProject.getRole() == UserRole.OWNER && userProjectDTO.role() != UserRole.OWNER) {
+            Long projectId = existingUserProject.getProject().getId();
+            long ownerCount = userProjectRepository.countByProjectIdAndRole(projectId, UserRole.OWNER);
+
+            if (ownerCount == 1) {
+                throw new CustomException(
+                        "Cannot change the role of this user because they are the only OWNER of the project.", ExceptionLevel.WARNING, HttpStatus.BAD_REQUEST
+                );
+            }
+        }
 
         existingUserProject.setRole(userProjectDTO.role());
 
--- src/main/java/com/workflow/scrumt/domain/repository/UserProjectRepository.java
+++ src/main/java/com/workflow/scrumt/domain/repository/UserProjectRepository.java
@@ -1,7 +1,9 @@
 package com.workflow.scrumt.domain.repository;
 
 import com.workflow.scrumt.domain.entity.UserProject;
+import com.workflow.scrumt.domain.enums.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserProjectRepository extends JpaRepository<UserProject, Long> {
+    long countByProjectIdAndRole(Long projectId, UserRole role);
 }
